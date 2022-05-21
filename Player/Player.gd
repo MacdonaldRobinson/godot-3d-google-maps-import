1c960dbfb4190a6ba035ba4a912d0b7f8cefb224
MacdonaldRobinson <maocnly2001@gmail.com>
2022-05-21 08:19:38 -0600

    updated export

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,7 +5,7 @@ export var springArmNodePath:NodePath
 onready var springArm:SpringArm = get_node(springArmNodePath)
 var is_movement_pressed = false
 var currentVector:Vector3 = Vector3.ZERO
-var gravity:float = -300
+var gravity:float = -200
 
 func _ready():
 	pass # Replace with function body.
@@ -19,8 +19,8 @@ func _physics_process(delta):
 	if is_movement_pressed:		
 		self.rotation.y = springArm.rotation.y
 		
-		if is_equal_approx(self.rotation.y, springArm.rotation.y):
-			is_movement_pressed = false
+	if is_equal_approx(self.rotation.y, springArm.rotation.y):
+		is_movement_pressed = false
 	
 	var zMovement:float = Input.get_action_strength("backward") - Input.get_action_strength("forward")
 	var xMovement:float = Input.get_action_strength("right") - Input.get_action_strength("left")
@@ -33,7 +33,7 @@ func _physics_process(delta):
 	newVector.y = gravity
 		
 	if Input.is_action_pressed("jump"):
-		newVector.y = 500
+		newVector.y = 300
 		
 	newVector = lerp(currentVector, newVector, 0.1)
 	currentVector = move_and_slide(newVector, Vector3.UP)
